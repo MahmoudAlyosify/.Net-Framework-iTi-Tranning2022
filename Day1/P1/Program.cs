a942b3c2ebb3f37f1c201456b5b7368c652d6ea3
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-16 17:45:00 +0200

    Day1

---
+++ Day1/P1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{/*
+  * Mahmoud Sayed Youssef
+  1- Write a program and ask the user to enter a number. 
+    The number should be between 1 to 10. If the user enters a valid number, display "Valid" on the console. Otherwise, display "Invalid".
+    (This logic is used a lot in applications where values entered into input boxes need to be validated.)
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Check;
+            int Num;
+            Console.WriteLine("Enter a number");
+            Check = int.TryParse(Console.ReadLine(), out Num);
+            if (Num >= 1 && Num <= 10 && Check) Console.WriteLine("Valid");
+            else Console.WriteLine("Invalid");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day1/P2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{/*
+  *Mahmoud Sayed Youssef
+  2- Write a program which takes two numbers from the console and displays the maximum of the two.
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Check1, Check2;
+            int Num1,Num2;
+            Console.WriteLine("Enter two numbers");
+            Check1 = int.TryParse(Console.ReadLine(), out Num1);
+            Check2 = int.TryParse(Console.ReadLine(), out Num2);
+           var Result= Num1 > Num2 & Check1 & Check2 ? ($"{Num1} is greater.") : ($"{Num2} is greater.");
+            Console.WriteLine(Result);
+            Console.ReadKey();
+        }
+    }
+}
