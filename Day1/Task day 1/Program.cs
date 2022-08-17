9fb3f8891179a7adf791ae8c52bb65cbb8a2173d
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-17 11:10:45 +0200

    Day 1

--- Day1/Task day 1/Program.cs
+++ Day1/Task day 1/Program.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Task_day_1
 {
+    /*
+     
+     Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+     */
     internal class Program
     {
         static void Main(string[] args)
