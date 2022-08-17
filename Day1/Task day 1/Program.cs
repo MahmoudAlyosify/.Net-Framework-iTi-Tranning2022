dbea298da91f743b52bc49c457f3a31680ed8d6a
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-17 11:07:50 +0200

    Day 1 Lab

---
+++ Day1/Task day 1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string OK = "ok";
+            string stringNumber;
+            int numericValue;
+            bool isNumber;
+            int sum = 0;
+            do
+            {
+                Console.WriteLine("Input a Number:");
+                stringNumber = Console.ReadLine();
+                isNumber = int.TryParse(stringNumber, out numericValue);
+                if (isNumber) sum += numericValue;
+                else if (!isNumber && stringNumber.ToLower() != OK) Console.WriteLine("Invalid input again\n");
+            }
+            while (stringNumber.ToLower() != OK);
+            Console.WriteLine($"The sum: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
