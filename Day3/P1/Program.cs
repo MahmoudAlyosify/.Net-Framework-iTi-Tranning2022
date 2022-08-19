57ec08bf86706914bb1833127fff517bd7043945
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-19 16:27:07 +0200

    Day3

---
+++ Day3/P1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{/*
+  Write a program and ask the user to enter a series of numbers separated by comma. Find the 
+maximum of the numbers and display it on the console. For example, if the user enters “5, 3, 8, 
+1, 4", the program should display 8
+  
+  */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool Check=false;
+            int Number;
+            List<int> Numbers = new List<int>();
+            Console.WriteLine("Enter series of Numbers separated by comma.");
+            string series=Console.ReadLine();
+            foreach (string N in series.Split(','))
+            {
+                Check=int.TryParse(N, out Number);
+                if(Check)Numbers.Add(Number);
+                else { Console.WriteLine("Your serie have char you must enter only Numbers OR you are type character in the end of series!!!");
+                    break;
+                }
+            }
+             if(Check)Console.WriteLine("The Maximum of the numbers is: " + Numbers.Max());
+            Console.ReadKey();
+        }
+    }
+}
