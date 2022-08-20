200e36ab46c19600bed2b0996479725b176ac6bf
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-20 23:40:44 +0200

    Day4

---
+++ Day4/P1/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{ //This class models a StackOverflow post
+    internal class Post
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime DateTimeOfCreation  { get; set; }
+        public int CountOfVoting { get; private set; }
+
+
+        public Post(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            DateTimeOfCreation = DateTime.UtcNow;
+            CountOfVoting = 0;
+        }
+
+        public void VoteUp()
+        {
+
+                CountOfVoting++;
+        }
+
+        public void VoteDown()
+        {
+            if(CountOfVoting > 0)
+                CountOfVoting--;
+            else
+            {
+                throw new Exception("The Counter of Voting is less than or equal 0");
+            }
+        }
+        public void PrintFullPost()
+        {
+            Console.WriteLine($"Title: {this.Title}");
+            Console.WriteLine($"Description: {this.Description}");
+            Console.WriteLine($"Date of Creatation: {this.DateTimeOfCreation.ToString("f")}");
+            Console.WriteLine($"The Count: {this.CountOfVoting}");
+        }
+    }
+}
---
+++ Day4/P2/Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2
+{
+    internal class Stack: IEnumerable
+    {
+        private object _object;
+        private List<object> list = new List<object>();
+
+        public IEnumerator GetEnumerator()
+        {
+            while (list.Any())
+                yield return Pop();
+        }
+
+        internal object Pop()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Cannot use .Pop() if list count equals 0.");
+
+            _object = list.FirstOrDefault();
+
+            list.RemoveAt(0);
+
+            return _object;
+        }
+
+        internal void Push(object obj)
+        {
+            _object = obj;
+
+            if (_object == null)
+                throw new InvalidOperationException("Cannot use .Push() if object is null.");
+
+            list.Insert(0, _object);
+        }
+
+        internal void Clear()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Cannot use .Clear() if list is empty.");
+
+            list.Clear();
+        }
+
+        public void Print()
+        {
+            if (list.Count == 0)
+                throw new InvalidOperationException("Stack is empty.");
+
+            foreach (var s in list)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
