a173b14a5af5e1f673040014d7a6d1dad7062025
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-20 12:52:52 +0200

    Edit Task1

--- Day4/Task1/Stopwatch.cs
+++ Day4/Task1/Stopwatch.cs
@@ -17,11 +17,16 @@ namespace Task1
 				Console.WriteLine("You started at {0}", TheStartTime.ToString("yyyy-MM-dd & HH:mm:ss"));	
 		}
 
+		public string Duration()
+        {
+			return (this.TheStopTime - this.TheStartTime).ToString();
+
+		}
 		public void Stop()
 		{
 				TheStopTime = DateTime.Now;
 				Console.WriteLine("You Stopped at {0}", TheStopTime.ToString("yyyy-MM-dd & HH:mm:ss"));
-				Console.WriteLine($"The Duration is :({(TheStopTime - TheStartTime)})");
+				Console.WriteLine($"The Duration is :({Duration()})");
 		}
 
 	}
