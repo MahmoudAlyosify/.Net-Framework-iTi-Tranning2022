8495d67966d866bcc5a8f61a1f76f3f47239491e
Mahmoud Sayed Youssef <59246305+MahmoudAlyosify@users.noreply.github.com>
2022-08-22 08:43:30 +0200

    Day5

---
+++ Day5/P1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Challenge 1. Write a program that reads a text file and displays the number of words.
+            var path = @"F:\.Net frame work iTi\Labs\.Net-Framework-iTi-Tranning2022\Day5\P1\Text.txt";
+
+            var wordNum = new List<string>(File.ReadAllText(path).Split(' '));
+
+            Console.WriteLine(wordNum.Count());
+
+            //Challenge 2. Write a program that reads a text file and displays the longest word in the file.
+
+            int wordLenght = 0;
+            var longestWord = "";
+
+            foreach (var word in wordNum)
+            {
+                if (word.Length > wordLenght)
+                {
+                    longestWord = word;
+                }
+            }
+            Console.WriteLine(longestWord);
+            Console.ReadKey();
+        }
+    }
+}
